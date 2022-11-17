0796040c8941765d53de9c028d4d9d6c830df357
Tarite Swapna <tariteswapna2002@gmail.com>
2022-11-17 22:34:20 +0530

    added

---
+++ src/main/java/in/kmit/dempproject/App.java
@@ -0,0 +1,13 @@
+package in.kmit.dempproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is swapna" );
+    }
+}
---
+++ src/test/java/in/kmit/dempproject/AppTest.java
@@ -0,0 +1,20 @@
+package in.kmit.dempproject;
+
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void apptestDisp()
+    {
+        System.out.println("\n.....Testing by Swapna ....\n");
+    }
+}
